d6da9a1ad3929cdf5d7f3add174d98c635e1680f
Samur Araujo <samuraraujo@gmail.com>
2012-06-06 21:04:51 +0200

    script

---
+++ experiments/HeuristicSearchApproach/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+jruby ./SIDER-TCM/experiment/experiment.rb
+jruby ./DAILYMED-SIDER/experiment/experiment.rb
+jruby ./DISEASOME-SIDER/experiment/experiment.rb
+jruby ./DRUGBANK-SIDER/experiment/experiment.rb
+jruby ./NYTIMES-FREEBASE-CORP/experiment/experiment.rb
+jruby ./NYTIMES-FREEBASE-GEO/experiment/experiment.rb
+jruby ./NYTIMES-FREEBASE-PER/experiment/experiment.rb 
+jruby ./SIDER-DAILYMED/experiment/experiment.rb
+jruby ./SIDER-DISEASOME/experiment/experiment.rb
+jruby ./SIDER-DRUGBANK/experiment/experiment.rb
